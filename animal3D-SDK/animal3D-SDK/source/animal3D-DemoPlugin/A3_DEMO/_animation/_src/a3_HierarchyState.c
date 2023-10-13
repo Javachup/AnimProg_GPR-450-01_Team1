81cf46125718afe32a69ee9e2c9681872fc3d3b2
anandas-b <79864434+anandas-b@users.noreply.github.com>
2023-10-12 20:55:07 -0400

    created a pose group once

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_HierarchyState.c
@@ -267,6 +267,7 @@ a3i32 a3hierarchyPoseGroupLoadHTR(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 						else if (strcmp(keyWord, "NumFrames") == 0)
 						{
 							int result = sscanf(line, "NumFrames %d", &numFrames);
+							a3hierarchyPoseGroupCreate(poseGroup_out, hierarchy_out, numFrames);
 							printf("Number of Frames: %d\n", numFrames);
 						}
 						else if (strcmp(keyWord, "DataFrameRate") == 0)
@@ -327,7 +328,6 @@ a3i32 a3hierarchyPoseGroupLoadHTR(a3_HierarchyPoseGroup* poseGroup_out, a3_Hiera
 				{
 					// set the base position
 					poseGroup_out->hierarchy = 0;
-					a3hierarchyPoseGroupCreate(poseGroup_out, hierarchy_out, numFrames);
 
 					char jointName[256];
 					a3f32 tX, tY, tZ, rX, rY, rZ, boneLength;
