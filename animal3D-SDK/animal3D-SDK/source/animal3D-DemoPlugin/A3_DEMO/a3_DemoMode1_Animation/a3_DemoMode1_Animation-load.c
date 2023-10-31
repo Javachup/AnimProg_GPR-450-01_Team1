edcd966052ba75c8f8d5befb9f4b5e5c900fe84f
Joseph Romanowski <joseph.romanowski@mymail.champlain.edu>
2023-10-30 22:14:32 -0400

    Uncomments set ops for nodes

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-load.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-load.c
@@ -202,9 +202,9 @@ void a3animation_init_animation(a3_DemoState const* demoState, a3_DemoMode1_Anim
 	// Blend Tree Stuff
 	a3blendNodePoolCreate(demoMode->nodePool, 3);
 
-	//a3blendNodeSetOp(demoMode->nodePool + 0, a3_BlendOpConcat);
-	//a3blendNodeSetOp(demoMode->nodePool + 1, a3_BlendOpLerp);
-	//a3blendNodeSetOp(demoMode->nodePool + 2, a3_BlendOpScale);
+	a3blendNodeSetOp(demoMode->nodePool->nodes + 0, a3_BlendOpConcat);
+	a3blendNodeSetOp(demoMode->nodePool->nodes + 1, a3_BlendOpLerp);
+	a3blendNodeSetOp(demoMode->nodePool->nodes + 2, a3_BlendOpScale);
 
 	a3hierarchyCreate(demoMode->blendTree, 3, 0); //initializing the tree
 	/*
