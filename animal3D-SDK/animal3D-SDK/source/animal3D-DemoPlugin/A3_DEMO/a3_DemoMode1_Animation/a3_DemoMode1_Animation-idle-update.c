50a9624a158f5c9dd459ef3f053538c35f4c8f57
Thomas Wagner <thomas.wagner@mymail.champlain.edu>
2023-11-13 20:07:20 -0500

    starting lookat function

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
@@ -228,6 +228,25 @@ void a3animation_update_applyEffectors(a3_DemoMode1_Animation* demoMode,
 			// make "look-at" matrix
 			// in this example, +Z is towards locator, +Y is up
 
+			/*
+			struct a3_DemoSceneObject
+	{
+		a3mat4 modelMat;	// model matrix: transform relative to scene
+		a3mat4 modelMatInv;	// inverse model matrix: scene relative to this
+		a3vec3 euler;		// euler angles for direct rotation control
+		a3vec3 position;	// scene position for direct control
+		a3vec3 scale;		// scale (not accounted for in update)
+		a3ui32 scaleMode;	// 0 = off; 1 = uniform; other = non-uniform (nightmare)
+		a3ui32 sceneGraphIndex;	// index in scene graph
+		*/
+			//^ sceneObject, which is the target for LookAt
+
+			/*
+			forward = normalize(from - to)
+			
+
+			*/
+
 			// ****TO-DO: 
 			// reassign resolved transforms to OBJECT-SPACE matrices
 			// resolve local and animation pose for affected joint
